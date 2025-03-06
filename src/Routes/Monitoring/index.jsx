36a4e8359b961c39d4e25559ed6c775f79ed649b
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2025-03-06 12:29:36 +0900

    change monitroing ui

--- src/Routes/Monitoring/index.jsx
+++ src/Routes/Monitoring/index.jsx
@@ -3,8 +3,8 @@ import { Button } from "../../Components/Samples/Button";
 
 export const Monitoring = () => {
   return (
-    <div className="w-full h-full flex gap-4 p-5">
-      <div className="w-1/2 h-full flex flex-col gap-4 overflow-hidden">
+    <div className="w-full h-full flex flex-col gap-4 p-5">
+      <div className="h-[55%] flex flex-col gap-4 overflow-hidden">
         <div className="flex-shrink-0 flex gap-3 items-center">
           <span>정상 이벤트</span>
           <Button text="초기화 버튼" height="30px" width="100px" />
@@ -23,7 +23,7 @@ export const Monitoring = () => {
           ]}
         />
       </div>
-      <div className="w-1/2 h-full flex flex-col gap-4 overflow-hidden">
+      <div className="h-[45%] flex flex-col gap-4 overflow-hidden">
         <div className="flex-shrink-0 flex gap-3 items-center">
           <span>에러 이벤트</span>
           <Button text="초기화 버튼" height="30px" width="100px" />
